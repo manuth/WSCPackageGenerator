3387e54b0c0e9988886a9ca5a3cbda79cd711870
Manuel Thalmann <m@nuth.ch>
2018-04-15 19:57:46 +0200

    Provide the functionality to automatically set the properties of a style

--- generators/app/templates/lib/Collections/StyleCollection.ts
+++ generators/app/templates/lib/Collections/StyleCollection.ts
@@ -1,5 +1,6 @@
 import Style from "../Customization/Style";
 import Instruction from "../Instruction";
+import { isNullOrUndefined } from "util";
 
 /**
  * Represents a set of style.
@@ -28,6 +29,34 @@ export default class StyleCollection<T extends Style> extends Array<T>
         for (let item of items)
         {
             item.Instruction = this.instruction;
+
+            if (this.instruction.Package)
+            {
+                if (isNullOrUndefined(item.Date))
+                {
+                    item.Date = this.instruction.Package.Date;
+                }
+
+                if (isNullOrUndefined(item.Version))
+                {
+                    item.Version = this.instruction.Package.Version;
+                }
+
+                if (isNullOrUndefined(item.Author.Name))
+                {
+                    item.Author.Name = this.instruction.Package.Author.Name;
+                }
+
+                if (isNullOrUndefined(item.Author.URL))
+                {
+                    item.Author.URL = this.instruction.Package.Author.URL;
+                }
+
+                if (isNullOrUndefined(item.License))
+                {
+                    item.License = this.instruction.Package.License;
+                }
+            }
         }
 
         return super.push(...items);
