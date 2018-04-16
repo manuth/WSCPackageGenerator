400ee4d7e81284ad3fc8bfc0b1153feff5d7b666
Manuel Thalmann <m@nuth.ch>
2018-04-16 21:43:28 +0200

    Fix incorrect import

--- generators/app/templates/lib/Listener.ts
+++ generators/app/templates/lib/Listener.ts
@@ -1,5 +1,5 @@
 import WSCEnvironment from "./WSCEnvironment";
-import Option from "../ControlPanel/Option";
+import Option from "./ControlPanel/Option";
 import { isNullOrUndefined } from "util";
 
 /**
