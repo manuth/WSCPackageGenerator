4a6aa7d99935cd2cf8adc97c8e65518ffc79b961
Manuel Thalmann <m@nuth.ch>
2018-04-17 13:45:02 +0200

    Extend the instruction-collection class

--- generators/app/templates/lib/Automation/InstructionCollection.ts
+++ generators/app/templates/lib/Automation/InstructionCollection.ts
@@ -1,5 +1,25 @@
 import Instruction from "./Instruction";
 import Package from "../Package";
+import FileMapping from "../FileMapping";
+import FilesInstruction from "../FilesInstruction";
+import Option from "../ControlPanel/Option";
+import OptionsInstruction from "../ControlPanel/OptionsInstruction";
+import SettingsNode from "../ControlPanel/SettingsNode";
+import TranslationNode from "../Globalization/TranslationNode";
+import EventListener from "../EventListener";
+import EventListenersInstruction from "../EventListenersInstruction";
+import TranslationsInstruction from "../Globalization/TranslationsInstruction";
+import Style from "../Customization/Style";
+import StyleInstruction from "../Customization/StyleInstruction";
+import TemplatesInstruction from "../Customization/TemplatesInstruction";
+import ACPTemplatesInstruction from "../Customization/ACPTemplatesInstruction";
+import TemplateListener from "../Customization/TemplateListener";
+import TemplateListenersInstruction from "../Customization/TemplateListenersInstruction";
+import Emoji from "../Customization/Emoji";
+import EmojisInstruction from "../Customization/EmojisInstruction";
+import OptionsDeletionInstruction from "../ControlPanel/OptionsDeletionInstruction";
+import EventListenersDeletionInstruction from "../EventListenersDeletionInstruction";
+import TemplateListenersDeletionInstruction from "../Customization/TemplateListenersDeletionInstruction";
 
 /**
  * Rerpesents a set of instructions.
@@ -34,6 +54,241 @@ export default class InstructionCollection extends Array<Instruction>
     public set Package(value: Package)
     {
         this.package = value;
+    }
+
+    /**
+     * Gets the mappings of the files provided by this collection.
+     */
+    public get FileMappings(): FileMapping[]
+    {
+        let result: FileMapping[] = [];
+
+        for (let instruction of this)
+        {
+            if ((instruction instanceof FilesInstruction) &&
+                (instruction.constructor === FilesInstruction))
+            {
+                result.push(...instruction.FileMappings);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Gets the options provided by this collection.
+     */
+    public get Options(): { [id: string]: Option }
+    {
+        let result: { [id: string]: Option } = { };
+
+        for (let instruction of this)
+        {
+            if (instruction instanceof OptionsInstruction)
+            {
+                Object.assign(result, instruction.Options);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Gets the names of the options to delete provided by this collection.
+     */
+    public get OptionsToDelete(): string[]
+    {
+        let result: string[] = [];
+
+        for (let instruction of this)
+        {
+            if (instruction instanceof OptionsDeletionInstruction)
+            {
+                result.push(...instruction.Names);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Gets the categories provided by this collection.
+     */
+    public get Categories(): SettingsNode[]
+    {
+        let result: SettingsNode[] = [];
+
+        for (let instruction of this)
+        {
+            if (instruction instanceof OptionsInstruction)
+            {
+                result.push(...instruction.Categories);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Gets the event-listeners provided by this collection.
+     */
+    public get EventListeners(): EventListener[]
+    {
+        let result: EventListener[] = [];
+
+        for (let instruction of this)
+        {
+            if (instruction instanceof EventListenersInstruction)
+            {
+                result.push(...instruction.EventListeners);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Gets the names of the event-listeners to delete provided by this collection.
+     */
+    public get EventListenersToDelete(): string[]
+    {
+        let result: string[] = [];
+
+        for (let instruction of this)
+        {
+            if (instruction instanceof EventListenersDeletionInstruction)
+            {
+                result.push(...instruction.Names);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Gets the translations provided by this collection.
+     */
+    public get Translations(): TranslationNode[]
+    {
+        let result: TranslationNode[] = [];
+
+        for (let instruction of this)
+        {
+            if (instruction instanceof TranslationsInstruction)
+            {
+                result.push(...instruction.TranslationNodes);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Gets the styles provided by this collection.
+     */
+    public get Styles(): Style[]
+    {
+        let result: Style[] = [];
+
+        for (let instruction of this)
+        {
+            if (instruction instanceof StyleInstruction)
+            {
+                result.push(instruction.Style);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Gets the templates provided by this collection.
+     */
+    public get Templates(): FileMapping[]
+    {
+        let result: FileMapping[] = [];
+
+        for (let instruction of this)
+        {
+            if (instruction instanceof TemplatesInstruction)
+            {
+                result.push(...instruction.FileMappings);
+            }
+        }
+        
+        return result;
+    }
+
+    /**
+     * Gets the acp-templates provided by this collection.
+     */
+    public get ACPTemplates(): FileMapping[]
+    {
+        let result: FileMapping[] = [];
+
+        for (let instruction of this)
+        {
+            if (instruction instanceof ACPTemplatesInstruction)
+            {
+                result.push(...instruction.FileMappings);
+            }
+        }
+        
+        return result;
+    }
+
+    /**
+     * Gets the template-listeners provided by this collection.
+     */
+    public get TemplateListeners(): TemplateListener[]
+    {
+        let result: TemplateListener[] = [];
+
+        for (let instruction of this)
+        {
+            if (instruction instanceof TemplateListenersInstruction)
+            {
+                result.push(...instruction.TemplateListeners);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Gets the names of the template-listeners to delete provided by this collection.
+     */
+    public get TemplateListenersToDelete(): string[]
+    {
+        let result: string[] = [];
+
+        for (let instruction of this)
+        {
+            if (instruction instanceof TemplateListenersDeletionInstruction)
+            {
+                result.push(...instruction.Names);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Gets the emojis provided by this collection.
+     */
+    public get Emojis(): Emoji[]
+    {
+        let result: Emoji[] = [];
+
+        for (let instruction of this)
+        {
+            if (instruction instanceof EmojisInstruction)
+            {
+                result.push(...instruction.Emojis);
+            }
+        }
+
+        return result;
     }
 
     public push(...items: Instruction[]): number
