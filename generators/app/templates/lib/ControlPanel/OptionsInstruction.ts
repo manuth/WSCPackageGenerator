e05566c3601f74844a98072a1de96a483ae7975d
Manuel Thalmann <m@nuth.ch>
2018-04-17 12:44:54 +0200

    Extend the options-instruction

--- generators/app/templates/lib/ControlPanel/OptionsInstruction.ts
+++ generators/app/templates/lib/ControlPanel/OptionsInstruction.ts
@@ -1,11 +1,13 @@
 import Instruction from "../Automation/Instruction";
 import SettingsNode from "./SettingsNode";
 import { isNullOrUndefined } from "util";
+import Option from "./Option";
+import TranslationNode from "../Globalization/TranslationNode";
 
 /**
  * Represents an instruction that provides options for the control-panel.
  */
-export default class OptionsInstruction extends Instruction
+export default class OptionsInstruction extends TranslationNode
 {
     /**
      * The categories and options provided by the instruction.
@@ -36,5 +38,29 @@ export default class OptionsInstruction extends Instruction
     public set SettingsNode(value: SettingsNode)
     {
         this.settingsNode = value;
+    }
+
+    /**
+     * Gets all options provided by the instruction.
+     */
+    public get Options(): { [id: string]: Option }
+    {
+        return this.settingsNode.GetOptions();
+    }
+
+    /**
+     * Gets all categories provided by the instruction.
+     */
+    public get Categories(): SettingsNode[]
+    {
+        return this.settingsNode.GetCategories();
+    }
+
+    /**
+     * Gets the translations provided by the instruction.
+     */
+    public get TranslationNodes(): TranslationNode[]
+    {
+        return this.SettingsNode.TranslationNodes;
     }
 }
