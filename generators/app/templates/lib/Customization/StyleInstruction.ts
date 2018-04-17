e2080d5a09c79c1e0587c115b9739c347673b075
Manuel Thalmann <m@nuth.ch>
2018-04-17 16:11:24 +0200

    Provide the functionality to automatically overwrite style-properties

--- generators/app/templates/lib/Customization/StyleInstruction.ts
+++ generators/app/templates/lib/Customization/StyleInstruction.ts
@@ -1,6 +1,7 @@
 import FileSystemInstruction from "../Automation/FileSystemInstruction";
 import Style from "./Style";
 import { isNullOrUndefined } from "util";
+import Package from "../Package";
 
 /**
  * Represents an instruction that provides a style.
@@ -31,5 +32,35 @@ export default class StyleInstruction extends FileSystemInstruction
     public get Style(): Style
     {
         return this.style;
+    }
+
+    public set Package(value: Package)
+    {
+        super.Package = value;
+
+        if (isNullOrUndefined(this.style.Date))
+        {
+            this.style.Date = value.Date;
+        }
+
+        if (isNullOrUndefined(this.style.Version))
+        {
+            this.style.Version = value.Version;
+        }
+        
+        if (isNullOrUndefined(this.style.Author.Name))
+        {
+            this.style.Author.Name = value.Author.Name;
+        }
+
+        if (isNullOrUndefined(this.style.Author.URL))
+        {
+            this.style.Author.URL = value.Author.URL;
+        }
+
+        if (isNullOrUndefined(this.style.License))
+        {
+            this.style.License = value.License;
+        }
     }
 }
