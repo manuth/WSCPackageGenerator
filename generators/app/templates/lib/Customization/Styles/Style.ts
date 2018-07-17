854dfcf7fbeb2a3abb7ccc235cb10e6d636e4208
Manuel Thalmann <m@nuth.ch>
2018-07-17 23:32:33 +0200

    Improve the way to handle style-variables

--- generators/app/templates/lib/Customization/Styles/Style.ts
+++ generators/app/templates/lib/Customization/Styles/Style.ts
@@ -1,10 +1,12 @@
 import * as FileSystem from "fs";
 import Component from "../../PackageSystem/Component";
 import IStyle from "./IStyle";
+import * as OS from "os";
 import * as Path from "path";
 import StyleInstruction from "./StyleInstruction";
 import ImageFolderDescriptor from "./ImageFolderDescriptor";
 import { isNullOrUndefined } from "util";
+import { parse } from "sass-variable-parser";
 
 /**
  * Represents a style for WoltLab Suite Core.
@@ -71,7 +73,7 @@ export default class Style extends Component
 
         if (!isNullOrUndefined(options.OverrideScssFile))
         {
-            this.overrideScss = FileSystem.readFileSync(options.OverrideScssFile).toString();
+            this.ParseOverrides(options.OverrideScssFile);
         }
     }
 
@@ -152,5 +154,200 @@ export default class Style extends Component
     public set OverrideScss(value: string)
     {
         this.overrideScss = value;
+    }
+
+    /**
+     * Parses an overrides-file.
+     * 
+     * @param fileName
+     * The name of the overrides-file.
+     */
+    protected ParseOverrides(fileName: string)
+    {
+        let variables: { [name: string]: string } = parse(
+            FileSystem.readFileSync(fileName).toString(),
+            {
+                camelCase: false,
+                cwd: Path.dirname(fileName)
+            });
+        
+        for (let name in variables)
+        {
+            switch (name)
+            {
+                case "wcfLayoutMinWidth":
+                case "wcfLayoutMaxWidth":
+                case "pageLogo":
+                case "pageLogoWidth":
+                case "pageLogoHeight":
+                case "pageLogoMobile":
+                case "wcfFontSizeDefault":
+                case "wcfFontSizeSmall":
+                case "wcfFontSizeHeadline":
+                case "wcfFontSizeSection":
+                case "wcfFontSizeTitle":
+                case "useGoogleFont":
+                case "wcfFontFamilyGoogle":
+                case "wcfFontFamilyFallback":
+
+                case "wcfHeaderBackground":
+                case "wcfHeaderText":
+                case "wcfHeaderLink":
+                case "wcfHeaderLinkActive":
+
+                case "wcfHeaderSearchBoxBackground":
+                case "wcfHeaderSearchBoxText":
+                case "wcfHeaderSearchBoxPlaceholder":
+                case "wcfHeaderSearchBoxPlaceholderActive":
+                case "wcfHeaderSearchBoxBackgroundActive":
+                case "wcfHeaderSearchBoxTextActive":
+                case "wcfHeaderMenuBackground":
+                case "wcfHeaderMenuLinkBackground":
+                case "wcfHeaderMenuLinkBackgroundActive":
+                case "wcfHeaderMenuLink":
+                case "wcfHeaderMenuLinkActive":
+
+                case "wcfHeaderMenuDropdownBackground":
+                case "wcfHeaderMenuDropdownLink":
+                case "wcfHeaderMenuDropdownBackgroundActive":
+                case "wcfHeaderMenuDropdownLinkActive":
+
+                case "wcfNavigationBackground":
+                case "wcfNavigationText":
+                case "wcfNavigationLink":
+                case "wcfNavigationLinkActive":
+
+                case "wcfSidebarBackground":
+                case "wcfSidebarText":
+                case "wcfSidebarLink":
+                case "wcfSidebarLinkActive":
+                
+                case "wcfSidebarDimmedText":
+                case "wcfSidebarDimmedLink":
+                case "$wcfSidebarDimmedLinkActive":
+
+                case "wcfSidebarHeadlineText":
+                case "wcfSidebarHeadlineLink":
+                case "wcfSidebarHeadlineLinkActive":
+
+                case "wcfContentBackground":
+                case "wcfContentBorder":
+                case "wcfContentBorderInner":
+                case "wcfContentText":
+                case "wcfContentLink":
+                case "wcfContentLinkActive":
+
+                case "wcfContentContainerBackground":
+                case "wcfContentContainerBorder":
+
+                case "wcfContentDimmedText":
+                case "wcfContentDimmedLink":
+                case "wcfContentDimmedLinkActive":
+                
+                case "wcfContentHeadlineBorder":
+                case "wcfContentHeadlineText":
+                case "wcfContentHeadlineLink":
+                case "wcfContentHeadlineLinkActive":
+
+                case "wcfTabularBoxBorderInner":
+                case "wcfTabularBoxHeadline":
+                case "wcfTabularBoxBackgroundActive":
+                case "wcfTabularBoxHeadlineActive":
+
+                case "wcfInputLabel":
+                case "wcfInputBackground":
+                case "wcfInputBorder":
+                case "wcfInputText":
+                case "wcfInputPlaceholder":
+                case "wcfInputPlaceholderActive":
+                case "wcfInputBackgroundActive":
+                case "wcfInputBorderActive":
+                case "wcfInputTextActive":
+
+                case "wcfInputDisabledBackground":
+                case "wcfInputDisabledBorder":
+                case "wcfInputDisabledText":
+                
+                case "wcfButtonBackground":
+                case "wcfButtonText":
+                case "wcfButtonBackgroundActive":
+                case "wcfButtonTextActive":
+
+                case "wcfButtonPrimaryBackground":
+                case "wcfButtonPrimaryText":
+                case "wcfButtonPrimaryBackgroundActive":
+                case "wcfButtonPrimaryTextActive":
+
+                case "wcfButtonDisabledBackground":
+                case "wcfButtonDisabledText":
+
+                case "wcfEditorButtonBackground":
+                case "wcfEditorButtonBackgroundActive":
+                case "wcfEditorButtonText":
+                case "wcfEditorButtonTextActive":
+                case "wcfEditorButtonTextDisabled":
+                
+                case "wcfDropdownBackground":
+                case "wcfDropdownBorderInner":
+                case "wcfDropdownText":
+                case "wcfDropdownLink":
+                case "wcfDropdownBackgroundActive":
+                case "wcfDropdownLinkActive":
+
+                case "wcfStatusInfoBackground":
+                case "wcfStatusInfoBorder":
+                case "wcfStatusInfoText":
+                case "wcfStatusInfoLink":
+                case "wcfStatusInfoLinkActive":
+
+                case "wcfStatusSuccessBackground":
+                case "wcfStatusSuccessBorder":
+                case "wcfStatusSuccessText":
+                case "wcfStatusSuccessLink":
+                case "wcfStatusSuccessLinkActive":
+                
+                case "wcfStatusWarningBackground":
+                case "wcfStatusWarningBorder":
+                case "wcfStatusWarningText":
+                case "wcfStatusWarningLink":
+                case "wcfStatusWarningLinkActive":
+                
+                case "wcfStatusErrorBackground":
+                case "wcfStatusErrorBorder":
+                case "wcfStatusErrorText":
+                case "wcfStatusErrorLink":
+                case "wcfStatusErrorLinkActive":
+                
+                case "wcfFooterBoxBackground":
+                case "wcfFooterBoxText":
+                case "wcfFooterBoxLink":
+                case "wcfFooterBoxLinkActive":
+                
+                case "wcfFooterBoxHeadlineText":
+                case "wcfFooterBoxHeadlineLink":
+                case "wcfFooterBoxHeadlineLinkActive":
+
+                case "wcfFooterBackground":
+                case "wcfFooterText":
+                case "wcfFooterLink":
+                case "wcfFooterLinkActive":
+
+                case "wcfFooterHeadlineText":
+                case "wcfFooterHeadlineLink":
+                case "wcfFooterHeadlineLinkActive":
+                
+                case "wcfFooterCopyrightBackground":
+                case "wcfFooterCopyrightText":
+                case "wcfFooterCopyrightLink":
+                case "wcfFooterCopyrightLinkActive":
+                    this.Variables[name] = variables[name];
+                    break;
+                
+                case "messageSidebarOrientation":
+                case "wcfFontLineHeight":
+                    this.OverrideScss += OS.EOL + `$${name}: ${variables[name]}`;
+                    break;
+            }
+        }
     }
 }
