b1d2f02beaada8ee7ebcd7a270794adfdc09b329
Manuel Thalmann <m@nuth.ch>
2018-08-24 22:21:49 +0200

    Remove all mocha-tests

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,22 +1,21 @@
-"use strict";
-import * as Path from "path";
-import assert = require("yeoman-assert");
-import * as helpers from "yeoman-test";
+// import * as Path from "path";
+// import assert = require("yeoman-assert");
+// import * as helpers from "yeoman-test";
 
-describe("WSCPackageGenerator", () =>
-{
-  before(() =>
-  {
-    return helpers.run(
-      Path.join(__dirname, "../generators/app")).withPrompts({ someAnswer: true });
-  });
+// describe("WSCPackageGenerator", () =>
+// {
+//   before(() =>
+//   {
+//     return helpers.run(
+//       Path.join(__dirname, "../generators/app")).withPrompts({ someAnswer: true });
+//   });
 
-  describe("creates files", () =>
-  {
-    let filename = "dummyfile.txt";
-    it(filename, () => 
-    {
-      assert.file([filename]);
-    });
-  });
-});
+//   describe("creates files", () =>
+//   {
+//     let filename = "dummyfile.txt";
+//     it(filename, () => 
+//     {
+//       assert.file([filename]);
+//     });
+//   });
+// });
