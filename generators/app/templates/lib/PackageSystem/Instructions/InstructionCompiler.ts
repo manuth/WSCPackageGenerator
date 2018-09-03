dfbe0b1e18e84302cdb10530ecdf9fa868fca85d
Manuel Thalmann <m@nuth.ch>
2018-09-04 00:23:33 +0200

    Improve the way to handle instruction-templates

--- generators/app/templates/lib/PackageSystem/Instructions/InstructionCompiler.ts
+++ generators/app/templates/lib/PackageSystem/Instructions/InstructionCompiler.ts
@@ -6,8 +6,16 @@ import { Instruction } from "./Instruction";
  */
 export class InstructionCompiler<T extends Instruction> extends Compiler<T>
 {
+    /**
+     * Gets the path of the template to copy to the package.
+     */
+    protected get TemplatePath(): string
+    {
+        return this.MakeTemplatePath(this.Item.TemplatePath);
+    }
+
     protected Compile()
     {
-        this.FileSystem.copyTpl(this.Item.TemplatePath, this.Item.DestinationPath, { Instruction: this.Item });
+        this.FileSystem.copyTpl(this.TemplatePath, this.Item.DestinationPath, { Instruction: this.Item });
     }
 }
