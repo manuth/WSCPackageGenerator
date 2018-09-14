f5fd748ff266bd2bf921f20774c66374bac40b9a
Manuel Thalmann <m@nuth.ch>
2018-09-14 09:59:19 +0200

    Add tests for the instruction-class

--- packageLib/src/tests/app.test.ts
+++ packageLib/src/tests/app.test.ts
@@ -1,6 +1,6 @@
 import * as assert from "assert";
+import { Instruction } from "../PackageSystem/Instructions/Instruction";
 import { Package } from "../PackageSystem/Package";
-import * as Path from "path";
 import { SQLInstruction } from "../PackageSystem/Instructions/Data/SQLInstruction";
 import { DOMParser } from "xmldom";
 
@@ -25,24 +25,70 @@ suite("WSCPackageGenerator", () =>
         "Instruction",
         () =>
         {
+            let instruction: Instruction;
+
+            suiteSetup(
+                () =>
+                {
+                    instruction = new SQLInstruction({ Source: "main.sql" });
+                    $package.InstallSet.push(instruction);
+                });
+
+            suite(
+                "Type",
+                () =>
+                {
+                    test(
+                        "Testing whether type equals the expected value...",
+                        () =>
+                        {
+                            assert.strictEqual(instruction.Type, "sql");
+                        });
+                });
+
             suite(
                 "XML",
                 () =>
                 {
-                    test(
-                        "Test whether the XML-code is built correctly... ",
+                    suite(
+                        "Testing whether the XML-code is built correctly... ",
                         () =>
                         {
-                            let sqlInstruction = new SQLInstruction({ Source: "main.sql" });
-                            $package.InstallSet.push(sqlInstruction);
-                            {
-                                let xml = new DOMParser().parseFromString(sqlInstruction.XML);
+                            let xml: Document;
 
-                                assert.strictEqual(xml.documentElement.tagName, "instruction");
-                                assert.strictEqual(xml.documentElement.hasAttribute("type"), true);
-                                assert.strictEqual(xml.documentElement.getAttribute("type"), "sql");
-                                assert.strictEqual(xml.documentElement.textContent, Path.join($package.InstallSet.Directory, sqlInstruction.FileName).replace(Path.sep, "/"));
-                            }
+                            suiteSetup(
+                                () =>
+                                {
+                                    xml = new DOMParser().parseFromString(instruction.XML);
+                                });
+
+                            test(
+                                "Testing whether the root-tag has the correct name...",
+                                () =>
+                                {
+                                    assert.strictEqual(xml.documentElement.tagName, "instruction");
+                                });
+
+                            test(
+                                'Testing whether the "type"-attribute is present...',
+                                () =>
+                                {
+                                    assert.strictEqual(xml.documentElement.hasAttribute("type"), true);
+                                });
+
+                            test(
+                                'Testing whether the "type"-attribute equals the "Type"-member of the instruction...',
+                                () =>
+                                {
+                                    assert.strictEqual(xml.documentElement.getAttribute("type"), instruction.Type);
+                                });
+
+                            test(
+                                "Testing whether the text of the tag equals the filename of the instruction...",
+                                () =>
+                                {
+                                    assert.strictEqual(xml.documentElement.textContent, instruction.FullName);
+                                });
                         });
                 });
         });
