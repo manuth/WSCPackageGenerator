e56bb160a8ce01c7f5c8929156ed8800d176a7d1
Manuel Thalmann <m@nuth.ch>
2018-09-15 10:52:46 +0200

    Add instructions for installing templates.

---
+++ packageLib/src/PackageSystem/Instructions/Customization/Presentation/ACPTemplateInstruction.ts
@@ -0,0 +1,21 @@
+import { ApplicationFileSystemInstruction } from "../../FileSystem/ApplicationFileSystemInstruction";
+import { IApplicationFileSystemInstructionOptions } from "../../FileSystem/IApplicationFileSystemInstructionOptions";
+
+/**
+ * Represents an instruction which provides templates for the control panel.
+ */
+export class ACPTemplateInstruction extends ApplicationFileSystemInstruction
+{
+    /**
+     * Initializes a new instance of the `ACPTemplateInstruction` class.
+     */
+    public constructor(options: IApplicationFileSystemInstructionOptions)
+    {
+        super(options);
+    }
+
+    public get Type(): string
+    {
+        return "acpTemplate";
+    }
+}
