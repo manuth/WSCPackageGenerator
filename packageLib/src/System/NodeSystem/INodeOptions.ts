98c43944dff80b20dddecbeb7cda0711a9216f3a
Manuel Thalmann <m@nuth.ch>
2018-09-15 12:25:06 +0200

    Add a class for representing recursive nodes

---
+++ packageLib/src/System/NodeSystem/INodeOptions.ts
@@ -0,0 +1,10 @@
+/**
+ * Provides options for the `Node` class.
+ */
+export interface INodeOptions
+{
+    /**
+     * The name of the node.
+     */
+    Name: string;
+}
---
+++ packageLib/src/System/NodeSystem/Node.ts
@@ -0,0 +1,82 @@
+import { INodeOptions } from "./INodeOptions";
+
+/**
+ * Represents a node.
+ */
+export abstract class Node
+{
+    /**
+     * The name of the node.
+     */
+    private name: string;
+
+    /**
+     * The parent of the node.
+     */
+    private parent: Node;
+
+    /**
+     * Initializes a new instance of the `Node` class.
+     */
+    public constructor(options: INodeOptions)
+    {
+        this.name = options.Name;
+    }
+
+    /**
+     * Gets the tree of the parents of the node.
+     */
+    protected get ParentTree(): Node[]
+    {
+        let result: Node[] = [];
+
+        for (let node: Node = this.Parent; node !== null; node = node.Parent)
+        {
+            result.push(node);
+        }
+
+        return result;
+    }
+
+    /**
+     * Gets or sets the name of the node.
+     */
+    public get Name(): string
+    {
+        return this.name;
+    }
+
+    public set Name(value: string)
+    {
+        this.name = value;
+    }
+
+    /**
+     * Gets the full name of the node.
+     */
+    public get FullName(): string
+    {
+        return [this as Node].concat(this.ParentTree).map((node: Node) => node.Name).join(".");
+    }
+
+    /**
+     * Gets or sets the parent of the node.
+     */
+    public get Parent(): Node
+    {
+        return this.parent;
+    }
+
+    public set Parent(value: Node)
+    {
+        this.parent = value;
+    }
+
+    /**
+     * Returns a string that represents the current object.
+     */
+    public toString(): string
+    {
+        return this.FullName;
+    }
+}
