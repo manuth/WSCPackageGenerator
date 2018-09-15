7e28b7cdffc06366c92d52fae42530560ea4fcce
Manuel Thalmann <m@nuth.ch>
2018-09-15 14:27:27 +0200

    Add classes for representing cron-jobs

---
+++ packageLib/src/System/Tasks/ICronJobOptions.ts
@@ -0,0 +1,44 @@
+import { Localizable } from "../../GLobalization/Localizable";
+import { ACPOption } from "../Options/ControlPanel/ACPOption";
+import { TimePeriod } from "./TimePeriod";
+
+/**
+ * Provides options for the `CronJob` class.
+ */
+export interface ICronJobOptions
+{
+    /**
+     * The name of the cron-job.
+     */
+    Name?: string;
+
+    /*
+     * The class-name of the cron-job.
+     */
+    ClassName: string;
+
+    /**
+     * The description of the cron-job.
+     */
+    Description?: Localizable;
+
+    /**
+     * A value indicating whether the cron-job can be disabled.
+     */
+    AllowDisable?: boolean;
+
+    /**
+     * A value indicating whether the cron-job can be edited.
+     */
+    AllowEdit?: boolean;
+
+    /**
+     * A set of options of which at least one must be enabled in order to execute the cron-job.
+     */
+    Options?: ACPOption[];
+
+    /**
+     * The period to execute the cron-job.
+     */
+    Period: TimePeriod;
+}
