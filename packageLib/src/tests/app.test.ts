cc9e881bfc0a3bb2002d303dde1d732379ddb407
Manuel Thalmann <m@nuth.ch>
2018-09-19 18:03:12 +0200

    Add unit-tests for the SassVariableParser

--- packageLib/src/tests/app.test.ts
+++ packageLib/src/tests/app.test.ts
@@ -1,7 +1,9 @@
 import * as assert from "assert";
+import * as dedent from "dedent";
 import * as FileSystem from "fs-extra";
 import * as Path from "path";
 import { ImageDirectoryDescriptor } from "../System/Customization/Presentation/Themes/ImageDirectoryDescriptor";
+import { SassVariableParser } from "../System/Customization/Presentation/Themes/SassVariableParser";
 import { TempDirectory } from "../System/FileSystem/TempDirectory";
 
 suite("WoltLab Suite Core Package Library", () =>
@@ -59,6 +61,96 @@ suite("WoltLab Suite Core Package Library", () =>
                 "Themes",
                 () =>
                 {
+                    suite(
+                        "SassVariableParser",
+                        () =>
+                        {
+                            let mainFile: string;
+                            let importFile: string;
+                            let tempDir: TempDirectory;
+
+                            let var1Name: string;
+                            let var1Value: string;
+                            let var2Name: string;
+                            let var2Value: string;
+                            let var3Name: string;
+
+                            let variablesWithoutImport: { [key: string]: string };
+                            let variablesWithImport: { [key: string]: string };
+
+                            suiteSetup(
+                                async () =>
+                                {
+                                    mainFile = "main.scss";
+                                    importFile = "import.scss";
+                                    tempDir = new TempDirectory();
+
+                                    var1Name = "a";
+                                    var1Value = "#000";
+                                    var2Name = "b";
+                                    var2Value = '"Hello World"';
+
+                                    await FileSystem.writeFile(tempDir.MakePath(mainFile), `$${var1Name}: ${var1Value};`);
+                                    await FileSystem.writeFile(
+                                        tempDir.MakePath(importFile),
+                                        dedent(
+                                            `
+                                            @import "${Path.basename(mainFile)}";
+                                            $${var2Name}: ${var2Value};
+                                            $${var3Name}: $${var1Name};`
+                                        ));
+
+                                    variablesWithoutImport = new SassVariableParser(tempDir.MakePath(mainFile)).Parse();
+                                    variablesWithImport = new SassVariableParser(tempDir.MakePath(importFile)).Parse();
+                                });
+
+                            suite(
+                                "Testing scss-files without import-statements...",
+                                () =>
+                                {
+                                    test(
+                                        "Checking whether expected variable is present...",
+                                        () => assert.strictEqual(var1Name in variablesWithoutImport, true));
+
+                                    test(
+                                        "Checking whether the value of the expected variable is correct...",
+                                        () => assert.strictEqual(variablesWithoutImport[var1Name], var1Value));
+                                });
+
+                            suite(
+                                "Testing scss-files with import-statements...",
+                                () =>
+                                {
+                                    test(
+                                        "Checking whether the expected variables are present...",
+                                        () =>
+                                        {
+                                            assert.strictEqual(var2Name in variablesWithImport, true);
+                                            assert.strictEqual(var3Name in variablesWithImport, true);
+                                        });
+
+                                    test(
+                                        "Checking whether independent variables have the correct value...",
+                                        () =>
+                                        {
+                                            assert.strictEqual(variablesWithImport[var2Name], var2Value);
+                                        });
+
+                                    test(
+                                        "Checking whether variables which depend on imports have the correct value...",
+                                        () =>
+                                        {
+                                            assert.strictEqual(variablesWithImport[var3Name], var1Value);
+                                        });
+                                });
+
+                            suiteTeardown(
+                                () =>
+                                {
+                                    tempDir.Dispose();
+                                });
+                        });
+
                     suite(
                         "ImageDirectoryDescriptor",
                         () =>
