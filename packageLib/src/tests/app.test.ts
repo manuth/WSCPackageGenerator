dc762e4ccced589562442ea1020b3d8c3ece4d2b
Manuel Thalmann <m@nuth.ch>
2018-09-19 13:35:59 +0200

    Add tests for the `TempDirectory` class

--- packageLib/src/tests/app.test.ts
+++ packageLib/src/tests/app.test.ts
@@ -1,5 +1,8 @@
 import * as assert from "assert";
+import * as FileSystem from "fs-extra";
+import * as Path from "path";
 import { ImageDirectoryDescriptor } from "../System/Customization/Presentation/Themes/ImageDirectoryDescriptor";
+import { TempDirectory } from "../System/FileSystem/TempDirectory";
 
 suite("WoltLab Suite Core Package Library", () =>
 {
@@ -7,6 +10,51 @@ suite("WoltLab Suite Core Package Library", () =>
         "System",
         () =>
         {
+            suite(
+                "FileSystem",
+                () =>
+                {
+                    suite(
+                        "TempDirectory",
+                        () =>
+                        {
+                            let tempDir: TempDirectory;
+                            let tempDirName: string;
+                            let tempFileName: string;
+
+                            suiteSetup(
+                                () =>
+                                {
+                                    tempDir = new TempDirectory();
+                                    tempDirName = tempDir.FileName;
+                                    tempFileName = "test.txt";
+                                });
+
+                            test(
+                                "Checking whether the temporary directory exists...",
+                                () => assert.strictEqual(FileSystem.pathExistsSync(tempDirName), true));
+
+                            test(
+                                "Checking whether files can be written to the temporary directory...",
+                                () => FileSystem.writeFileSync(tempDir.MakePath(tempFileName), "test"));
+
+                            test(
+                                "Checking whether the file written to the temporary directory exists...",
+                                () => assert.strictEqual(FileSystem.existsSync(Path.join(tempDirName, tempFileName)), true));
+
+                            test(
+                                "Checking whether the `TempDirectory`-object can be disposed...",
+                                () => assert.doesNotThrow(() => tempDir.Dispose()));
+
+                            test(
+                                "Checking whether the temporary directory has been deleted...",
+                                () =>
+                                {
+                                    assert.strictEqual(FileSystem.pathExistsSync(tempDirName), false);
+                                });
+                        });
+                });
+
             suite(
                 "Themes",
                 () =>
