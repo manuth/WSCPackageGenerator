e1692de7b2ae8718bac4768123d400fdc72f20f5
Manuel Thalmann <m@nuth.ch>
2018-09-19 11:13:38 +0200

    Add tests for the `ImageDirectoryDescriptor` class

--- packageLib/src/tests/app.test.ts
+++ packageLib/src/tests/app.test.ts
@@ -1,4 +1,51 @@
+import * as assert from "assert";
+import { ImageDirectoryDescriptor } from "../System/Customization/Presentation/Themes/ImageDirectoryDescriptor";
 
 suite("WoltLab Suite Core Package Library", () =>
 {
+    suite(
+        "ImageDirectoryDescriptor",
+        () =>
+        {
+            let customFileName: string = "example.tar";
+            let customDestination: string = "dist";
+
+            let imageDirectory: ImageDirectoryDescriptor = new ImageDirectoryDescriptor(
+                {
+                    Source: "example"
+                });
+
+            let customImageDirectory: ImageDirectoryDescriptor = new ImageDirectoryDescriptor(
+                {
+                    Source: "example",
+                    FileName: customFileName,
+                    DestinationRoot: customDestination
+                });
+
+            suite(
+                "FileName",
+                () =>
+                {
+                    test(
+                        'Check whether the `FileName`-property is set to "images.tar" when no filename is specified...',
+                        () => assert.strictEqual(imageDirectory.FileName, "images.tar"));
+
+                    test(
+                        "Check whether the `FileName`-property is set properly when a filename is specified...",
+                        () => assert.strictEqual(customImageDirectory.FileName, customFileName));
+                });
+
+            suite(
+                "DestinationRoot",
+                () =>
+                {
+                    test(
+                        "Check whether `DestinationRoot` is set to `Source` when no destination-root is specified...",
+                        () => assert.strictEqual(imageDirectory.DestinationRoot, imageDirectory.Source));
+
+                    test(
+                        "Check whether `DestinationRoot` is set properly when a destination-root is specified...",
+                        () => assert.strictEqual(customImageDirectory.DestinationRoot, customDestination));
+                });
+        });
 });
