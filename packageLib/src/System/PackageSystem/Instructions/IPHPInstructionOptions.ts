74ded8f6bad7e55ff0caf2b42375a6a92173c326
Manuel Thalmann <m@nuth.ch>
2018-09-21 10:05:11 +0200

    Fix the `PHPInstruction` class

--- packageLib/src/System/PackageSystem/Instructions/IPHPInstructionOptions.ts
+++ packageLib/src/System/PackageSystem/Instructions/IPHPInstructionOptions.ts
@@ -1,7 +1,9 @@
+import { IInstructionOptions } from "./IInstructionOptions";
+
  /**
   * Provides options for the `PHPInstruction` class.
   */
-export interface IPHPInstructionOptions
+export interface IPHPInstructionOptions extends IInstructionOptions
 {
     /**
      * The applicatino to load the php-file from.
--- packageLib/src/System/PackageSystem/Instructions/PHPInstruction.ts
+++ packageLib/src/System/PackageSystem/Instructions/PHPInstruction.ts
@@ -1,12 +1,29 @@
+import { Instruction } from "./Instruction";
+import { IPHPInstructionOptions } from "./IPHPInstructionOptions";
+
 /**
  * Represents an instruction which executes `php`-code.
  */
-export class PHPInstruction
+export class PHPInstruction extends Instruction
 {
     /**
      * The applicatino to load the php-file from.
      */
     private application: string;
+
+    /**
+     * Initializes a new instance of the `PHPInstruction` class.
+     */
+    public constructor(options: IPHPInstructionOptions)
+    {
+        super(options);
+        this.Appliaction = options.Application;
+    }
+
+    public get Type(): string
+    {
+        return "script";
+    }
 
     /**
      * Gets or sets the applicatino to load the php-file from.
