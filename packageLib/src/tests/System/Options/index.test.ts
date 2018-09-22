b8ab58055bbb83c11bbec9d55cfb93ca3f2c7d4c
Manuel Thalmann <m@nuth.ch>
2018-09-22 14:52:24 +0200

    Add tests for the `Option`-class

---
+++ packageLib/src/tests/System/Options/index.test.ts
@@ -0,0 +1,6 @@
+suite(
+    "Options",
+    () =>
+    {
+        require("./Option.test");
+    });
--- packageLib/src/tests/System/index.test.ts
+++ packageLib/src/tests/System/index.test.ts
@@ -6,6 +6,7 @@ suite(
         require("./FileSystem/index.test");
         require("./NodeSystem/index.test");
         require("./Globalization/index.test");
+        require("./Options/index.test");
         require("./PackageSystem/index.test");
         require("./Tasks/index.test");
         require("./Themes/index.test");
