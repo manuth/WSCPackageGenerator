d7751191acbeee2a0b05a005cacf79210facc962
Manuel Thalmann <m@nuth.ch>
2018-09-22 23:25:53 +0200

    Rework the compiler-class

--- packageLib/src/System/Compilation/Compiler.ts
+++ packageLib/src/System/Compilation/Compiler.ts
@@ -1,8 +1,5 @@
 import * as ChildProcess from "child_process";
-import * as memFs from "mem-fs";
-import * as memFsEditor from "mem-fs-editor";
 import * as Path from "path";
-import { TempDirectory } from "../FileSystem/TempDirectory";
 
 /**
  * Provides the functionality to compile a component.
@@ -18,11 +15,6 @@ export abstract class Compiler<T>
      * The path to save the compiled item to.
      */
     private destinationPath: string = "";
-
-    /**
-     * The filesystem-editor for generating the output of the compiler.
-     */
-    private fileSystem: memFsEditor.memFsEditor.Editor = memFsEditor.create(memFs.create());
 
     /**
      * Initializes a new instance of the `Compiler` class.
@@ -54,22 +46,6 @@ export abstract class Compiler<T>
     public set DestinationPath(value: string)
     {
         this.destinationPath = value;
-    }
-
-    /**
-     * Gets the path to save temporary files to.
-     */
-    public get TempPath(): TempDirectory
-    {
-        return new TempDirectory();
-    }
-
-    /**
-     * Gets the filesystem-editor for generating the output of the compiler.
-     */
-    public get FileSystem(): memFsEditor.memFsEditor.Editor
-    {
-        return this.fileSystem;
     }
 
     /**
