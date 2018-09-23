a6d589af3b699db6cd9046ae795515220f67523b
Manuel Thalmann <m@nuth.ch>
2018-09-23 14:04:23 +0200

    Add a method for creating xml-documents

--- packageLib/src/System/Serialization/XML.ts
+++ packageLib/src/System/Serialization/XML.ts
@@ -1,8 +1,35 @@
+import { DOMParser } from "xmldom";
+
 /**
  * Provides utilities for the xml-serialization.
  */
 export class XML
 {
+    /**
+     * Creates a new xml-document.
+     *
+     * @param tagName
+     * The name of the tag of the `documentElement`.
+     */
+    public static CreateDocument(tagName: string): Document
+    {
+        let result: Document = new DOMParser().parseFromString(`<${tagName} />`);
+        result.insertBefore(
+            result.createProcessingInstruction("xml", 'version="1.0" encoding="UTF-8"'),
+            result.documentElement);
+
+        return result;
+    }
+
+    /**
+     * Beautifies xml-code.
+     *
+     * @param xml
+     * The xml-code to beautify.
+     *
+     * @returns
+     * Beautified xml-code.
+     */
     public static Prettify(xml: string): string
     {
         let formatted: string = "";
