bead02a2e0a6e9bfcef1bdf732f08fba765f8d0e
Manuel Thalmann <m@nuth.ch>
2018-09-24 13:03:48 +0200

    Rework the ThemeInstructionCompiler

--- packageLib/src/System/Compilation/ThemeInstructionCompiler.ts
+++ packageLib/src/System/Compilation/ThemeInstructionCompiler.ts
@@ -1,10 +1,7 @@
-import * as FileSystem from "fs-extra";
-import { isNullOrUndefined } from "util";
 import { TempDirectory } from "../FileSystem/TempDirectory";
-import { TempFile } from "../FileSystem/TempFile";
 import { ThemeInstruction } from "../PackageSystem/Instructions/Customization/Presentation/ThemeInstruction";
-import { XML } from "../Serialization/XML";
 import { InstructionCompiler } from "./InstructionCompiler";
+import { ThemeCompiler } from "./ThemeCompiler";
 
 /**
  * Provides the functionality to compile styles
@@ -24,39 +21,15 @@ export class ThemeInstructionCompiler extends InstructionCompiler<ThemeInstructi
 
     protected async Compile(): Promise<void>
     {
-        let styleDirectory: TempDirectory = new TempDirectory();
-        {
-            let variableFile: TempFile = new TempFile();
-            {
-                let variables: { [key: string]: string } = {};
-                let variableDocument: Document = XML.CreateDocument("variables");
-                Object.assign(variables, this.Item.Theme.Variables);
+        let tempDir: TempDirectory = new TempDirectory();
+        let themeDir: TempDirectory = new TempDirectory();
+        let themeCompiler: ThemeCompiler = new ThemeCompiler(this.Item.Theme);
 
-                if (!isNullOrUndefined(this.Item.Theme.CustomScss))
-                {
-                    variables.indivicualScss = this.Item.Theme.CustomScss;
-                }
-
-                if (!isNullOrUndefined(this.Item.Theme.ScssOverride))
-                {
-                    variables.overrideScss = this.Item.Theme.ScssOverride;
-                }
-
-                variableDocument.documentElement.setAttribute("xmlns", "http://www.woltlab.com");
-                variableDocument.documentElement.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
-                variableDocument.documentElement.setAttribute("xsi:schemaLocation", "http://www.woltlab.com http://www.woltlab.com/XSD/tornado/styleVariables.xsd");
-
-                for (let variableName in this.Item.Theme.Variables)
-                {
-                    let variableNode: Element = variableDocument.createElement("variable");
-                    variableNode.setAttribute("name", variableName);
-                    variableNode.appendChild(variableDocument.createTextNode(this.Item[variableName]));
-                    variableDocument.documentElement.appendChild(variableNode);
-                }
-
-                await FileSystem.writeFile(variableFile.FileName, XML.Prettify(new XMLSerializer().serializeToString(variableDocument)));
-                await this.CopyTemplate(variableFile.FileName, styleDirectory.MakePath("variables.xml"));
-            }
-        }
+        themeCompiler.DestinationPath = tempDir.FileName;
+        await themeCompiler.Execute();
+        this.CopyTemplate(tempDir.FileName, themeDir.FileName);
+        this.Compress(themeDir.FileName, this.DestinationPath);
+        tempDir.Dispose();
+        themeDir.Dispose();
     }
 }
