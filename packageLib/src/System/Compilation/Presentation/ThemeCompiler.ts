22923c4f2ccf4f63624116669a90ff6c0c08b5fd
Manuel Thalmann <m@nuth.ch>
2018-09-30 23:45:14 +0200

    Divide the ThemeCompiler into two classes

--- packageLib/src/System/Compilation/Presentation/ThemeCompiler.ts
+++ packageLib/src/System/Compilation/Presentation/ThemeCompiler.ts
@@ -1,13 +1,11 @@
-import * as FileSystem from "fs-extra";
 import { isNullOrUndefined } from "util";
-import { XMLSerializer } from "xmldom";
 import { Theme } from "../../Customization/Presentation/Themes/Theme";
-import { XML } from "../../Serialization/XML";
 import { Compiler } from "../Compiler";
+import { ThemeFileCompiler } from "./ThemeFileCompiler";
 import { ThemeVariableCompiler } from "./ThemeVariableCompiler";
 
 /**
- * Provides the functionality to compile theme.
+ * Provides the functionality to compile themes.
  */
 export class ThemeCompiler extends Compiler<Theme>
 {
@@ -17,7 +15,10 @@ export class ThemeCompiler extends Compiler<Theme>
     private variableFileName: string = "variables.xml";
 
     /**
-     * Initializes a new instance of the `ThemeCompiler` class.
+     * Initializes a new instance of the `ThemeCompiler<T>` class.
+     *
+     * @param item
+     * The item to compile.
      */
     public constructor(item: Theme, variableFileName?: string)
     {
@@ -57,124 +58,15 @@ export class ThemeCompiler extends Compiler<Theme>
             variables.overrideScss = this.Item.ScssOverride;
         }
 
-        let document: Document = XML.CreateDocument("style");
-        let general: Element = document.createElement("general");
+        let themeFileCompiler: ThemeFileCompiler = new ThemeFileCompiler(this.Item, this.VariableFileName);
+        themeFileCompiler.DestinationPath = this.MakeDestinationPath("style.xml");
+        await themeFileCompiler.Execute();
+
+        if (Object.keys(variables).length > 0)
         {
-            for (let locale of this.Item.DisplayName.GetLocales())
-            {
-                let name: Element = document.createElement("stylename");
-
-                if (locale !== "inv")
-                {
-                    name.setAttribute("language", locale);
-                }
-
-                name.appendChild(document.createTextNode(this.Item.DisplayName.Data[locale]));
-                general.appendChild(name);
-            }
-
-            let $package: Element = document.createElement("packageName");
-            $package.appendChild(document.createTextNode(this.Item.Instruction.Collection.Package.Identifier));
-            general.appendChild($package);
-
-            for (let locale of this.Item.Description.GetLocales())
-            {
-                let description: Element = document.createElement("description");
-
-                if (locale !== "inv")
-                {
-                    description.setAttribute("languag", locale);
-                }
-
-                description.appendChild(document.createTextNode(this.Item.Description.Data[locale]));
-                general.appendChild(description);
-            }
-
-            let api: Element = document.createElement("apiVersion");
-            api.appendChild(document.createTextNode("3.1"));
-            general.appendChild(api);
-
-            let date: Element = document.createElement("date");
-            date.appendChild(
-                document.createTextNode(
-                    this.Item.CreationDate.getFullYear() + "-" +
-                    (this.Item.CreationDate.getMonth() + 1).toString().padStart(2, "0") + "-" +
-                    this.Item.CreationDate.getDate().toString().padStart(2, "0")));
-            general.appendChild(date);
-
-            if (!isNullOrUndefined(this.Item.Thumbnail))
-            {
-                let thumbnail: Element = document.createElement("image");
-                thumbnail.appendChild(document.createTextNode(this.Item.Thumbnail));
-                general.appendChild(thumbnail);
-            }
-
-            if (!isNullOrUndefined(this.Item.HighResThumbnail))
-            {
-                let highResThumbnail: Element = document.createElement("image2x");
-                highResThumbnail.appendChild(document.createTextNode(this.Item.HighResThumbnail));
-                general.appendChild(highResThumbnail);
-            }
-
-            if (!isNullOrUndefined(this.Item.CoverPhoto))
-            {
-                let coverPhoto: Element = document.createElement("coverPhoto");
-                coverPhoto.appendChild(document.createTextNode(this.Item.CoverPhoto));
-                general.appendChild(coverPhoto);
-            }
-
-            if (!isNullOrUndefined(this.Item.License))
-            {
-                let license: Element = document.createElement("license");
-                license.appendChild(document.createTextNode(this.Item.License));
-                general.appendChild(license);
-            }
+            let variableCompiler: ThemeVariableCompiler = new ThemeVariableCompiler(variables);
+            variableCompiler.DestinationPath = this.MakeDestinationPath(this.VariableFileName);
+            await variableCompiler.Execute();
         }
-        document.documentElement.appendChild(general);
-
-        let author: Element = document.createElement("author");
-        {
-            if (!isNullOrUndefined(this.Item.Author.Name))
-            {
-                let name: Element = document.createElement("authorname");
-                name.appendChild(document.createTextNode(this.Item.Author.Name));
-                author.appendChild(name);
-            }
-
-            if (!isNullOrUndefined(this.Item.Author.URL))
-            {
-                let url: Element = document.createElement("authorurl");
-                url.appendChild(document.createTextNode(this.Item.Author.URL));
-            }
-        }
-        document.documentElement.appendChild(author);
-
-        if (Object.keys(variables).length > 0 || !isNullOrUndefined(this.Item.Images))
-        {
-            let files: Element = document.createElement("files");
-            {
-                if (Object.keys(variables).length > 0)
-                {
-                    let variableCompiler: ThemeVariableCompiler = new ThemeVariableCompiler(variables);
-                    variableCompiler.DestinationPath = this.MakeDestinationPath(this.VariableFileName);
-                    await variableCompiler.Execute();
-
-                    let variableFile: Element = document.createElement("variables");
-                    variableFile.appendChild(document.createTextNode(this.VariableFileName));
-                    files.appendChild(variableFile);
-                }
-
-                if (!isNullOrUndefined(this.Item.Images))
-                {
-                    let images: Element = document.createElement("images");
-                    images.setAttribute("path", this.Item.Images.DestinationRoot);
-                    images.appendChild(document.createTextNode(this.Item.Images.FileName));
-                    files.appendChild(images);
-                }
-            }
-            document.documentElement.appendChild(files);
-        }
-
-        await FileSystem.writeFile(this.MakeDestinationPath("style.xml"), XML.Format(new XMLSerializer().serializeToString(document)));
     }
 }
---
+++ packageLib/src/System/Compilation/Presentation/ThemeFileCompiler.ts
@@ -0,0 +1,173 @@
+import { isNullOrUndefined } from "util";
+import { Theme } from "../../Customization/Presentation/Themes/Theme";
+import { WoltLabXMLCompiler } from "../WoltLabXMLCompiler";
+
+/**
+ * Provides the functionality to compile theme-files.
+ */
+export class ThemeFileCompiler extends WoltLabXMLCompiler<Theme>
+{
+    /**
+     * The name to save the variables to.
+     */
+    private variableFileName: string;
+
+    /**
+     * Initializes a new instance of the `ThemeFileCompiler<T>` class.
+     *
+     * @param item
+     * The item to compile.
+     */
+    public constructor(item: Theme, variableFileName: string)
+    {
+        super(item);
+        this.VariableFileName = variableFileName;
+    }
+
+    protected get TagName(): string
+    {
+        return "style";
+    }
+
+    protected get SchemaLocation(): string
+    {
+        return "https://www.woltlab.com/XSD/vortex/style.xsd";
+    }
+
+    protected get XMLElement(): Document
+    {
+        let document: Document = super.XMLElement;
+
+        let general: Element = document.createElement("general");
+        {
+            for (let locale of this.Item.DisplayName.GetLocales())
+            {
+                let name: Element = document.createElement("stylename");
+
+                if (locale !== "inv")
+                {
+                    name.setAttribute("language", locale);
+                }
+
+                name.appendChild(document.createTextNode(this.Item.DisplayName.Data[locale]));
+                general.appendChild(name);
+            }
+
+            let $package: Element = document.createElement("packageName");
+            $package.appendChild(document.createTextNode(this.Item.Instruction.Collection.Package.Identifier));
+            general.appendChild($package);
+
+            for (let locale of this.Item.Description.GetLocales())
+            {
+                let description: Element = document.createElement("description");
+
+                if (locale !== "inv")
+                {
+                    description.setAttribute("language", locale);
+                }
+
+                description.appendChild(document.createTextNode(this.Item.Description.Data[locale]));
+                general.appendChild(description);
+            }
+
+            let api: Element = document.createElement("apiVersion");
+            api.appendChild(document.createTextNode("3.1"));
+            general.appendChild(api);
+
+            let date: Element = document.createElement("date");
+            date.appendChild(
+                document.createTextNode(
+                    this.Item.CreationDate.getFullYear() + "-" +
+                    (this.Item.CreationDate.getMonth() + 1).toString().padStart(2, "0") + "-" +
+                    this.Item.CreationDate.getDate().toString().padStart(2, "0")));
+            general.appendChild(date);
+
+            if (!isNullOrUndefined(this.Item.Thumbnail))
+            {
+                let thumbnail: Element = document.createElement("image");
+                thumbnail.appendChild(document.createTextNode(this.Item.Thumbnail));
+                general.appendChild(thumbnail);
+            }
+
+            if (!isNullOrUndefined(this.Item.HighResThumbnail))
+            {
+                let highResThumbnail: Element = document.createElement("image2x");
+                highResThumbnail.appendChild(document.createTextNode(this.Item.HighResThumbnail));
+                general.appendChild(highResThumbnail);
+            }
+
+            if (!isNullOrUndefined(this.Item.CoverPhoto))
+            {
+                let coverPhoto: Element = document.createElement("coverPhoto");
+                coverPhoto.appendChild(document.createTextNode(this.Item.CoverPhoto));
+                general.appendChild(coverPhoto);
+            }
+
+            if (!isNullOrUndefined(this.Item.License))
+            {
+                let license: Element = document.createElement("license");
+                license.appendChild(document.createTextNode(this.Item.License));
+                general.appendChild(license);
+            }
+        }
+        document.documentElement.appendChild(general);
+
+        let author: Element = document.createElement("author");
+        {
+            if (!isNullOrUndefined(this.Item.Author.Name))
+            {
+                let name: Element = document.createElement("authorname");
+                name.appendChild(document.createTextNode(this.Item.Author.Name));
+                author.appendChild(name);
+            }
+
+            if (!isNullOrUndefined(this.Item.Author.URL))
+            {
+                let url: Element = document.createElement("authorurl");
+                url.appendChild(document.createTextNode(this.Item.Author.URL));
+            }
+        }
+        document.documentElement.appendChild(author);
+
+        let files: Element = document.createElement("files");
+        {
+            if (
+                Object.keys(this.Item.Variables).length > 0 ||
+                !isNullOrUndefined(this.Item.CustomScss) ||
+                !isNullOrUndefined(this.Item.ScssOverride))
+            {
+                let variables: Element = document.createElement("variables");
+                variables.appendChild(document.createTextNode(this.VariableFileName));
+                files.appendChild(variables);
+            }
+
+            if (!isNullOrUndefined(this.Item.Images))
+            {
+                let images: Element = document.createElement("images");
+                images.setAttribute("path", this.Item.Images.DestinationRoot);
+                images.appendChild(document.createTextNode(this.Item.Images.FileName));
+                files.appendChild(images);
+            }
+        }
+
+        if (files.childNodes.length > 0)
+        {
+            document.documentElement.appendChild(files);
+        }
+
+        return document;
+    }
+
+    /**
+     * Gets or sets the filename to save variables to.
+     */
+    public get VariableFileName(): string
+    {
+        return this.variableFileName;
+    }
+
+    public set VariableFileName(value: string)
+    {
+        this.variableFileName = value;
+    }
+}
