dc8e4ffca170ec053eb907cb8ec8794b28f2c7e5
Manuel Thalmann <m@nuth.ch>
2018-10-02 15:12:30 +0200

    Add tests for the `InstructionCompiler`

--- packageLib/src/tests/System/Compilation/Instructions/index.test.ts
+++ packageLib/src/tests/System/Compilation/Instructions/index.test.ts
@@ -1,6 +1,7 @@
 suite(
     "Instructions", () =>
     {
+        require("./InstructionCompiler.test");
         require("./FileInstructionCompiler.test");
         require("./CronJobInstructionCompiler.test");
         require("./ThemeInstructionCompiler.test");
