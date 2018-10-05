ffd394319c42bccb3242bd9a575bdf6416fa8902
Manuel Thalmann <m@nuth.ch>
2018-10-05 09:59:45 +0200

    Complete the ICategory interface

--- packageLib/src/System/Options/ICategory.ts
+++ packageLib/src/System/Options/ICategory.ts
@@ -1,3 +1,4 @@
+import { Localization } from "../Globalization/Localization";
 import { INodeItem } from "../NodeSystem/INodeItem";
 
 /**
@@ -6,7 +7,17 @@ import { INodeItem } from "../NodeSystem/INodeItem";
 export interface ICategory extends INodeItem
 {
     /**
-     * A value for ordering the category.
+     * Gets or sets a value for ordering the category.
      */
     ShowOrder: number;
+
+    /**
+     * Gets the human-readable name of the category.
+     */
+    DisplayName: Localization;
+
+    /**
+     * Gets the description of the category.
+     */
+    Description: Localization;
 }
