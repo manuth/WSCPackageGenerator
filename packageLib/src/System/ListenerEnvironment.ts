79fb50c62db55851be725490ea529c300b910358
Manuel Thalmann <m@nuth.ch>
2018-10-07 01:54:08 +0200

    Rework the ListenerEnvironment-enum

--- packageLib/src/System/ListenerEnvironment.ts
+++ packageLib/src/System/ListenerEnvironment.ts
@@ -4,22 +4,12 @@
 export enum ListenerEnvironment
 {
     /**
-     * Indicates no environment.
-     */
-    None = 0,
-
-    /**
      * Indicates the front-end which is the default environment.
      */
-    FrontEnd = 1 << 0,
+    FrontEnd = "user",
 
     /**
      * Indicates the back-end, like, for example, the control panel.
      */
-    BackEnd = 1 << 2,
-
-    /**
-     * Indicates all possible environments.
-     */
-    All = (1 << 0) + (1 << 2)
+    BackEnd = "admin"
 }
