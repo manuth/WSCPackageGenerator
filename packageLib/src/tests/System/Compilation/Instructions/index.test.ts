941dfee73b13af0ea7966c9296dd258526471ce2
Manuel Thalmann <m@nuth.ch>
2018-10-06 23:00:17 +0200

    Add tests for the ACPOptionInstructionCompiler

--- packageLib/src/tests/System/Compilation/Instructions/index.test.ts
+++ packageLib/src/tests/System/Compilation/Instructions/index.test.ts
@@ -4,6 +4,7 @@ suite(
         require("./InstructionCompiler.test");
         require("./FileInstructionCompiler.test");
         require("./LocalizationInstructionCompiler.test");
+        require("./ACPOptionInstructionCompiler.test");
         require("./CronJobInstructionCompiler.test");
         require("./ThemeInstructionCompiler.test");
         require("./BBCodeInstructionCompiler.test");
