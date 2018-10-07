d116a41dbdef92ff44731ac3c84f6535961585cc
Manuel Thalmann <m@nuth.ch>
2018-10-08 00:09:50 +0200

    Add a component for compiling event-listener files

---
+++ packageLib/src/System/PackageSystem/Instructions/Events/EventListenerInstruction.ts
@@ -0,0 +1,28 @@
+import { EventListener } from "../../../Events/EventListener";
+import { IEventListenerOptions } from "../../../Events/IEventListenerOptions";
+import { IListenerInstructionOptions } from "./IListenerInstructionOptions";
+import { ListenerInstruction } from "./ListenerInstruction";
+
+/**
+ * Represents an instruction which provides event-listeners.
+ */
+export class EventListenerInstruction extends ListenerInstruction<EventListener, IEventListenerOptions>
+{
+    /**
+     * Initializes a new instance of the `EventListenerInstruction` class.
+     */
+    public constructor(options: IListenerInstructionOptions<IEventListenerOptions>)
+    {
+        super(
+            options,
+            (opts: IEventListenerOptions) =>
+            {
+                return new EventListener(opts);
+            });
+    }
+
+    public get Type(): string
+    {
+        return "eventListener";
+    }
+}
