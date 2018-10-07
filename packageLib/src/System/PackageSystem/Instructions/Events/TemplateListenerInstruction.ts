6ac35e9d73374f83990728c5a211f71dfeb32a0f
Manuel Thalmann <m@nuth.ch>
2018-10-08 01:27:38 +0200

    Add an instruction for providing template-listeners

---
+++ packageLib/src/System/PackageSystem/Instructions/Events/TemplateListenerInstruction.ts
@@ -0,0 +1,28 @@
+import { ITemplateListenerOptions } from "../../../Customization/Presentation/ITemplateListenerOptions";
+import { TemplateListener } from "../../../Customization/Presentation/TemplateListener";
+import { IListenerInstructionOptions } from "./IListenerInstructionOptions";
+import { ListenerInstruction } from "./ListenerInstruction";
+
+/**
+ * Represents an instruction which provides template-listeners.
+ */
+export class TemplateListenerInstruction extends ListenerInstruction<TemplateListener, ITemplateListenerOptions>
+{
+    /**
+     * Initializes a new instance of the `TemplateListenerInstruction` class.
+     */
+    public constructor(options: IListenerInstructionOptions<ITemplateListenerOptions>)
+    {
+        super(
+            options,
+            (opts: ITemplateListenerOptions) =>
+            {
+                return new TemplateListener(opts);
+            });
+    }
+
+    public get Type(): string
+    {
+        return "templateListener";
+    }
+}
