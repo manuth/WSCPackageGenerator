1ccdd6fbb66660f5f535f7788bdc9f5ce818d939
Manuel Thalmann <m@nuth.ch>
2018-10-09 13:10:02 +0200

    Add tests for the FileDescriptor class

--- packageLib/src/tests/System/PackageSystem/index.test.ts
+++ packageLib/src/tests/System/PackageSystem/index.test.ts
@@ -2,6 +2,7 @@ suite(
     "PackageSystem",
     () =>
     {
+        require("./FileDescriptor.test");
         require("./ModuleInfo.test");
         require("./Package.test");
         require("./Instructions/index.test");
