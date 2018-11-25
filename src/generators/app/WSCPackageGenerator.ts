9314dd667c159a00b810e92db35da8b64a46f177
Manuel Thalmann <m@nuth.ch>
2018-11-25 18:25:55 +0100

    Rework the package.json template

--- src/generators/app/WSCPackageGenerator.ts
+++ src/generators/app/WSCPackageGenerator.ts
@@ -409,7 +409,15 @@ export class WSCPackageGenerator extends Generator<IWSCPackageSettings>
         this.fs.copy(this.templatePath("_.vscode"), this.destinationPath(".vscode"));
         this.fs.copy(this.modulePath("WSCPackage", "src"), this.destinationPath(this.sourcePath()));
         this.fs.copy(this.templatePath("_.gitignore"), this.destinationPath(".gitignore"));
-        this.fs.copyTpl(this.templatePath("package.json.ejs"), this.destinationPath("package.json"), { Settings: this.Settings });
+        this.fs.copyTpl(
+            this.templatePath("package.json.ejs"),
+            this.destinationPath("package.json"),
+            {
+                Name: this.Settings[WSCPackageSetting.Name],
+                Description: this.Settings[WSCPackageSetting.Description] || "",
+                Author: this.Settings[WSCPackageSetting.Author],
+                HomePage: this.Settings[WSCPackageSetting.HomePage]
+            });
         this.fs.copyTpl(this.templatePath("README.md.ejs"), this.destinationPath("README.md"), { Settings: this.Settings });
         this.fs.copy(this.templatePath("_tsconfig.json"), this.destinationPath("tsconfig.json"));
         this.fs.copy(this.templatePath("wsc-package-quickstart.md"), this.destinationPath("wsc-package-quickstart.md"));
