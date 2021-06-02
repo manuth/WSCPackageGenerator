851095ed146167c9f952e7f93d9a3dd819a5ef0f
Manuel Thalmann <m@nuth.ch>
2021-06-02 12:51:25 +0200

    Make use of the new package-constants

--- src/WoltLabPackageFileMapping.ts
+++ src/WoltLabPackageFileMapping.ts
@@ -1,7 +1,7 @@
 import { GeneratorOptions, IGenerator } from "@manuth/extended-yo-generator";
 import { TSProjectPackageFileMapping } from "@manuth/generator-ts-project";
 import { Package } from "@manuth/package-json-editor";
-import { join } from "upath";
+import { Constants } from "./Core/Constants";
 import { IWoltLabGeneratorSettings } from "./IWoltLabGeneratorSettings";
 import { WoltLabSettingKey } from "./WoltLabSettingKey";
 
@@ -39,10 +39,7 @@ export class WoltLabPackageFileMapping<TSettings extends IWoltLabGeneratorSettin
             result.Dependencies.Remove(woltLabDependency);
         }
 
-        result.Dependencies.Add(
-            woltLabDependency,
-            new Package(join(__dirname, "..", "package.json")).AllDependencies.Get(woltLabDependency));
-
+        result.Dependencies.Add(woltLabDependency, Constants.Dependencies.Get(woltLabDependency));
         return result;
     }
 }
