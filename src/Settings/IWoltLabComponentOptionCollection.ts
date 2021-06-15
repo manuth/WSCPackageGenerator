971ca2191c1c411fb1e15ae71ccd12c4dd9860a1
Manuel Thalmann <m@nuth.ch>
2021-06-15 21:24:12 +0200

    Add default options for unknown components

--- src/Settings/IWoltLabComponentOptionCollection.ts
+++ src/Settings/IWoltLabComponentOptionCollection.ts
@@ -1,6 +1,12 @@
+import { IWoltLabComponentOptions } from "./IWoltLabComponentOptions";
+
 /**
  * Provides options for the components.
  */
 export interface IWoltLabComponentOptionCollection
 {
+    /**
+     * Provides generic component-options.
+     */
+    [componentName: string]: IWoltLabComponentOptions;
 }
