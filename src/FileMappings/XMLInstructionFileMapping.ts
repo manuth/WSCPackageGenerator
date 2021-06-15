8be1faa701dda17bc2882df9909ed8fe5d1024f1
Manuel Thalmann <m@nuth.ch>
2021-06-15 23:25:31 +0200

    Add a base class for xml-file instructions

---
+++ src/FileMappings/XMLInstructionFileMapping.ts
@@ -0,0 +1,45 @@
+import { GeneratorOptions } from "@manuth/extended-yo-generator";
+import { IInstructionOptions } from "@manuth/woltlab-compiler";
+import { ObjectLiteralExpression, printNode, ts } from "ts-morph";
+import { InstructionComponent } from "../Components/InstructionComponent";
+import { IWoltLabComponentOptions } from "../Settings/IWoltLabComponentOptions";
+import { IWoltLabGeneratorSettings } from "../Settings/IWoltLabGeneratorSettings";
+import { InstructionFileMapping } from "./InstructionFileMapping";
+
+/**
+ * Provides the functionality to generate bbcode instruction files.
+ */
+export abstract class XMLInstructionFileMapping<TSettings extends IWoltLabGeneratorSettings, TOptions extends GeneratorOptions, TComponentOptions extends IWoltLabComponentOptions> extends InstructionFileMapping<TSettings, TOptions, TComponentOptions>
+{
+    /**
+     * Initializes a new instance of the {@link BBCodeInstructionFileMapping `BBCodeInstructionFileMapping<TSettings, TOptions, TComponentOptions>`} class.
+     *
+     * @param component
+     * The component to create an instruction-file for.
+     */
+    public constructor(component: InstructionComponent<TSettings, TOptions, TComponentOptions>)
+    {
+        super(component);
+    }
+
+    /**
+     * Gets the name of the file to write the instruction's xml-code to.
+     */
+    protected abstract get XMLFileName(): string;
+
+    /**
+     * Gets the options to pass to the instruction-constructor.
+     */
+    protected override get InstructionOptions(): ObjectLiteralExpression
+    {
+        let options = super.InstructionOptions;
+
+        options.addPropertyAssignment(
+            {
+                name: nameof<IInstructionOptions>((instruction) => instruction.FileName),
+                initializer: printNode(ts.factory.createStringLiteral(this.XMLFileName))
+            });
+
+        return options;
+    }
+}
