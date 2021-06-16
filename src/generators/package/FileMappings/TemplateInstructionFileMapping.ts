a8a22eb641b0a8117ee1b2ef95c4c02d4c68bd1b
Manuel Thalmann <m@nuth.ch>
2021-06-16 13:24:34 +0200

    Make `TemplateInstructionFileMapping` extend `LocalFileInstructionMapping`

--- src/generators/package/FileMappings/TemplateInstructionFileMapping.ts
+++ src/generators/package/FileMappings/TemplateInstructionFileMapping.ts
@@ -1,15 +1,15 @@
 import { join } from "path";
 import { GeneratorOptions } from "@manuth/extended-yo-generator";
 import { ObjectLiteralExpression, printNode, SourceFile, ts } from "ts-morph";
-import { InstructionComponent } from "../../../Components/InstructionComponent";
-import { InstructionFileMapping } from "../../../FileMappings/InstructionFileMapping";
+import { LocalInstructionComponent } from "../../../Components/LocalInstructionComponent";
+import { LocalFileInstructionMapping } from "../../../FileMappings/LocalFileInstructionMapping";
 import { IWoltLabGeneratorSettings } from "../../../Settings/IWoltLabGeneratorSettings";
 import { ITemplateComponentOptions } from "../Components/ITemplateComponentOptions";
 
 /**
  * Provides the functionality to generate template instruction files.
  */
-export class TemplateInstructionFileMapping<TSettings extends IWoltLabGeneratorSettings, TOptions extends GeneratorOptions, TComponentOptions extends ITemplateComponentOptions> extends InstructionFileMapping<TSettings, TOptions, TComponentOptions>
+export class TemplateInstructionFileMapping<TSettings extends IWoltLabGeneratorSettings, TOptions extends GeneratorOptions, TComponentOptions extends ITemplateComponentOptions> extends LocalFileInstructionMapping<TSettings, TOptions, TComponentOptions>
 {
     /**
      * Initializes a new instance of the {@link TemplateInstructionFileMapping `TemplateInstructionFileMapping<TSettings, TOptions, TComponentOptions>`} class.
@@ -17,7 +17,7 @@ export class TemplateInstructionFileMapping<TSettings extends IWoltLabGeneratorS
      * @param component
      * The component to create an instruction-file for.
      */
-    public constructor(component: InstructionComponent<TSettings, TOptions, TComponentOptions>)
+    public constructor(component: LocalInstructionComponent<TSettings, TOptions, TComponentOptions>)
     {
         super(component);
     }
@@ -29,17 +29,12 @@ export class TemplateInstructionFileMapping<TSettings extends IWoltLabGeneratorS
     {
         let options = super.InstructionOptions;
 
-        options.addPropertyAssignments(
-            [
-                {
-                    name: nameof(this.Component.ComponentOptions.Application),
-                    initializer: printNode(ts.factory.createStringLiteral(this.Component.ComponentOptions.Application))
-                },
-                {
-                    name: nameof(this.Component.ComponentOptions.Source),
-                    initializer: this.GetPathJoin(this.Component.ComponentOptions.Source)
-                }
-            ]);
+        options.insertPropertyAssignment(
+            options.getProperty(nameof(this.Component.ComponentOptions.Source)).getChildIndex(),
+            {
+                name: nameof(this.Component.ComponentOptions.Application),
+                initializer: printNode(ts.factory.createStringLiteral(this.Component.ComponentOptions.Application))
+            });
 
         return options;
     }
