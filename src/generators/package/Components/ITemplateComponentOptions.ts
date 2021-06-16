3107069aa76f7960a8205b7aa823c37aac47cc2f
Manuel Thalmann <m@nuth.ch>
2021-06-16 13:07:53 +0200

    Make `ITemplateComponentOptions` inherit `ILocalComponentOptions`

--- src/generators/package/Components/ITemplateComponentOptions.ts
+++ src/generators/package/Components/ITemplateComponentOptions.ts
@@ -1,19 +1,14 @@
-import { IWoltLabComponentOptions } from "../../../Settings/IWoltLabComponentOptions";
+import { ILocalComponentOptions } from "../../../Settings/ILocalComponentOptions";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { TemplateComponent } from "./TemplateComponent";
 
 /**
  * Provides options for the {@link TemplateComponent `TemplateComponent<TSettings, TOptions, TComponentOptions>`}
  */
-export interface ITemplateComponentOptions extends IWoltLabComponentOptions
+export interface ITemplateComponentOptions extends ILocalComponentOptions
 {
     /**
      * The application to upload the templates to.
      */
     Application: string;
-
-    /**
-     * The path to load the templates from.
-     */
-    Source: string;
 }
