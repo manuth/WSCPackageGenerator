15e22fb2144f7440188ac58b8e3e19efd970828f
Manuel Thalmann <m@nuth.ch>
2021-09-07 14:58:26 +0200

    Remove unnecessary file

--- src/Settings/WoltLabComponentType.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { WoltLabComponent } from "../Components/WoltLabComponent";
-
-/**
- * Represents a type of a {@link WoltLabComponent `WoltLabComponent<TSettings, TOptions>`}.
- */
-export enum WoltLabComponentType
-{
-}
