8cf71a39c216cd93142285e69a2034556808e71d
Manuel Thalmann <m@nuth.ch>
2022-09-14 20:34:16 +0200

    Add a class for testing instruction files

---
+++ src/tests/InstructionFileMappingSuite.ts
@@ -0,0 +1,293 @@
+import { doesNotReject, doesNotThrow, ok } from "assert";
+import { spawnSync } from "child_process";
+import { fileURLToPath } from "url";
+import { FileMapping, Generator, GeneratorOptions } from "@manuth/extended-yo-generator";
+import { TestContext } from "@manuth/extended-yo-generator-test";
+import { TypeScriptCreatorMapping } from "@manuth/generator-ts-project";
+import { TypeScriptFileMappingTester } from "@manuth/generator-ts-project-test";
+import { Instruction } from "@manuth/woltlab-compiler";
+import { Context } from "mocha";
+import npmWhich from "npm-which";
+import { SourceFile } from "ts-morph";
+import { InstructionComponent } from "../Components/InstructionComponent.js";
+import { InstructionFileMapping } from "../FileMappings/InstructionFileMapping.js";
+import { IWoltLabComponentOptions } from "../Settings/IWoltLabComponentOptions.js";
+import { IWoltLabSettings } from "../Settings/IWoltLabSettings.js";
+import { TestSuite } from "./TestSuite.js";
+
+/**
+ * Represents a typescript file mapping.
+ */
+declare abstract class TestFileMapping extends TypeScriptCreatorMapping<any, any>
+{
+    /**
+     * @inheritdoc
+     *
+     * @param sourceFile
+     * The source-file to process.
+     *
+     * @returns
+     * The processed data.
+     */
+    public override Transform(sourceFile: SourceFile): Promise<SourceFile>;
+}
+
+/**
+ * Provides the functionality to register tests for {@link InstructionFileMapping `InstructionFileMapping<TSettings, TOptions, TComponentOptions>`} classes.
+ */
+export abstract class InstructionFileMappingSuite<TSettings extends IWoltLabSettings, TOptions extends GeneratorOptions, TGenerator extends Generator<TSettings, TOptions>, TComponentOptions extends IWoltLabComponentOptions, TFileMapping extends InstructionFileMapping<TSettings, TOptions, TComponentOptions>> extends TestSuite
+{
+    /**
+     * A component for testing the generator.
+     */
+    private context: TestContext<TGenerator, TOptions>;
+
+    /**
+     * The generator of the file mapping to test.
+     */
+    private generator: TGenerator;
+
+    /**
+     * A component for testing the file mapping.
+     */
+    private tester: TypeScriptFileMappingTester<TGenerator, TSettings, TOptions, TFileMapping>;
+
+    /**
+     * Initializes a new instance of the {@link InstructionFileMappingSuite `InstructionFileMappingSuite<TSettings, TOptions, TGenerator, TComponentOptions, TFileMapping>`} class.
+     *
+     * @param context
+     * The test context.
+     */
+    public constructor(context: TestContext<TGenerator, TOptions>)
+    {
+        super();
+        this.context = context;
+    }
+
+    /**
+     * Gets a component for testing the generator.
+     */
+    protected get Context(): TestContext<TGenerator, TOptions>
+    {
+        return this.context;
+    }
+
+    /**
+     * Gets the generator for testing the generator.
+     */
+    protected get Generator(): TGenerator
+    {
+        return this.generator;
+    }
+
+    /**
+     * Gets the options of the file mapping to test.
+     */
+    protected get FileMappingOptions(): TFileMapping
+    {
+        return this.Tester.FileMappingOptions;
+    }
+
+    /**
+     * Gets the file mapping to test.
+     */
+    protected get FileMapping(): FileMapping<TSettings, TOptions>
+    {
+        return this.Tester.FileMapping;
+    }
+
+    /**
+     * Gets the component of the file mapping.
+     */
+    protected get Component(): InstructionComponent<TSettings, TOptions, TComponentOptions>
+    {
+        return this.FileMappingOptions["Component"];
+    }
+
+    /**
+     * Gets a component for testing the file mapping.
+     */
+    protected get Tester(): TypeScriptFileMappingTester<TGenerator, TSettings, TOptions, TFileMapping>
+    {
+        return this.tester;
+    }
+
+    /**
+     * Creates the file mapping to test.
+     *
+     * @returns
+     * The file mapping to test.
+     */
+    protected abstract CreateFileMapping(): TFileMapping;
+
+    /**
+     * Creates a component for testing the file mapping.
+     *
+     * @returns
+     * A component for testing the file mapping.
+     */
+    protected CreateTester(): TypeScriptFileMappingTester<TGenerator, TSettings, TOptions, TFileMapping>
+    {
+        return new TypeScriptFileMappingTester(this.Generator, this.CreateFileMapping());
+    }
+
+    /**
+     * Registers the tests for the {@link InstructionFileMapping.Transform `Transform`} method.
+     */
+    protected RegisterTransformSuite(): void
+    {
+        suite.only(
+            nameof<TestFileMapping>((fileMapping) => fileMapping.Transform),
+            () =>
+            {
+                let self = this;
+
+                suiteSetup(
+                    async function()
+                    {
+                        return self.TransformSuiteSetup(this);
+                    });
+
+                suiteTeardown(
+                    async function()
+                    {
+                        return self.TransformSuiteTeardown(this);
+                    });
+
+                setup(
+                    async function()
+                    {
+                        return self.TransformSetup(this);
+                    });
+
+                teardown(
+                    async function()
+                    {
+                        return self.Teardown(this);
+                    });
+
+                this.RegisterTransformTests();
+            });
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @param context
+     * The mocha context.
+     */
+    protected override async SuiteSetup(context: Context): Promise<void>
+    {
+        context.timeout(5 * 60 * 1000);
+        this.generator = await this.Context.Generator;
+        this.tester = this.CreateTester();
+
+        spawnSync(
+            npmWhich(fileURLToPath(new URL(".", import.meta.url))).sync("npm"),
+            [
+                "install",
+                "--silent"
+            ],
+            {
+                cwd: this.Generator.destinationPath(),
+                stdio: "ignore"
+            });
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected override RegisterTests(): void
+    {
+        this.RegisterTransformSuite();
+    }
+
+    /**
+     * Prepares the suite for the {@link InstructionFileMapping.Transform `Transform`} method.
+     *
+     * @param context
+     * The mocha context.
+     */
+    protected async TransformSuiteSetup(context: Context): Promise<void>
+    { }
+
+    /**
+     * Releases all resources of the suite for the {@link InstructionFileMapping.Transform `Transform`} method.
+     *
+     * @param context
+     * The mocha context.
+     */
+    protected async TransformSuiteTeardown(context: Context): Promise<void>
+    { }
+
+    /**
+     * Prepares each test for the {@link InstructionFileMapping.Transform `Transform`} method.
+     *
+     * @param context
+     * The mocha context.
+     */
+    protected async TransformSetup(context: Context): Promise<void>
+    {
+        context.timeout(10 * 1000);
+
+        await this.Tester.DumpOutput(
+            await this.FileMappingOptions["Transform"](await this.FileMappingOptions.GetSourceObject()));
+    }
+
+    /**
+     * Releases the resources of each test for the {@link InstructionFileMapping.Transform `Transform`} method.
+     *
+     * @param context
+     * The mocha context.
+     */
+    protected async TransformTeardown(context: Context): Promise<void>
+    { }
+
+    /**
+     * Registers the tests for the {@link InstructionFileMapping.Transform `Transform`} method.
+     */
+    protected RegisterTransformTests(): void
+    {
+        let self = this;
+
+        test(
+            "Checking whether valid typescript code is emitted…",
+            async function()
+            {
+                this.timeout(30 * 1000);
+                this.slow(15 * 1000);
+                await doesNotReject(() => self.Tester.Import());
+            });
+
+        test(
+            "Checking whether a variable with the expected name is exposed…",
+            async function()
+            {
+                this.timeout(30 * 1000);
+                this.slow(15 * 1000);
+                ok(self.Component.VariableName in await self.Tester.Import());
+            });
+
+        test(
+            `Checking whether the exported variable inherits the \`${nameof(Instruction)}\` class…`,
+            async function()
+            {
+                this.timeout(30 * 1000);
+                this.slow(15 * 1000);
+                let exportedValue = (await self.Tester.Import())[self.Component.VariableName];
+                let classCandidates: any[] = [];
+
+                for (let candidate = exportedValue.constructor; candidate !== null; candidate = Object.getPrototypeOf(candidate))
+                {
+                    classCandidates.push(candidate);
+                }
+
+                ok(
+                    classCandidates.some(
+                        (candidate) =>
+                        {
+                            return `${candidate}` === Instruction.toString();
+                        }));
+            });
+    }
+}
